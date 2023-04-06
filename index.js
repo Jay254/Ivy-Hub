ddd6a91e6cca3b51405bb10c0704eb8da5992da0
Jay254 <jyegon504@gmail.com>
2023-04-06 15:45:39 +0300

    Commit #4

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', () =>{
+   const form = document.getElementById('form');
+   const collegeName = document.getElementsByClassName('collegeName');
+   const appRound = document.getElementsByClassName('appRound');
+   const appPlan = document.getElementsByClassName('appPlan');
    
+   //form.addEventListener('submit', handleSubmit)
+
 
 })
