ea3c5cdb423d05d15d3f2a910f03709f7b5faaa0
Jay254 <jyegon504@gmail.com>
2023-04-07 20:16:33 +0300

    Commit #17

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
+//fires when initial HTML document has completely loaded and parsed
 document.addEventListener('DOMContentLoaded', () => {
+    //grabbing html elements from the DOM and storing them in variables
     const form = document.getElementById('form');
     const collegeName = document.getElementById('collegeName');
     const appRound = document.getElementById('appRound');
     const appPlan = document.getElementById('appPlan');
+    const cover = document.getElementById('cover');
+
+    const addedColleges = new Set();  //keeps track of colleges already in our college list
+    //an array containing names of all the Ivy league colleges in the US
     const ivyLeague = ['Harvard', 'Yale', 'Princeton', 'Columbia', 'Dartmouth', 'Brown', 'Cornell', 'Upenn']
   
-    const cover = document.getElementById('cover');
-    const addedColleges = new Set();
+    form.addEventListener('submit', handleSubmit);//handles submit events in our form element
   
-    form.addEventListener('submit', handleSubmit);
-  
-    cover.addEventListener('click', handleRemove);
+    cover.addEventListener('click', handleRemove);//handles click events in our cover element
   
     function handleSubmit(event) {
-      event.preventDefault();
+      event.preventDefault();//cancels default submit action if not given explicit instructions
+
+      //stores our form input values into variables
       let college = collegeName.value;
       console.log(college);
       let round = appRound.value;
@@ -21,14 +26,17 @@ document.addEventListener('DOMContentLoaded', () => {
       let plan = appPlan.value;
       console.log(plan);
 
+      //Checks if the input college name is part of the Ivy league schools and alerts the user if it isn't
       if(ivyLeague.includes(college)===false){
         return alert('Enter an Ivy League Institution!');
       }
 
+      //Checks if the input college name is already in our list and alerts the user if so
       if (addedColleges.has(college)) {
         return alert('This college has already been added to the list.');
       }
   
+      //fetch request to display the user's input college on our DOM
       fetch(`http://localhost:3000/${college}`)
         .then(res => res.json())
         .then(data => {
