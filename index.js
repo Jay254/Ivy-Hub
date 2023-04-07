239d9d53eeee7f26246ce938152a5474f1bebb3a
Jay254 <jyegon504@gmail.com>
2023-04-07 13:43:27 +0300

    Commit #9

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ document.addEventListener('DOMContentLoaded', () =>{
    const collegeName = document.getElementById('collegeName');
    const appRound = document.getElementById('appRound');
    const appPlan = document.getElementById('appPlan');
-//    const h2 = document.createElement('h2');
-//    const h3 = document.createElement('h3');
-//    const p = document.createElement('p');
-//    const a = document.createElement('a');
-//    const img = document.createElement('img');
-//    const p1 = document.createElement('p');
 
    const cover = document.getElementById('cover')
    
@@ -16,12 +10,14 @@ document.addEventListener('DOMContentLoaded', () =>{
 
    function handleSubmit(event){
     event.preventDefault();
-     
-    console.log(collegeName.value)
-    console.log(appRound.value)
-    console.log(appPlan.value)
+    let college = collegeName.value
+    console.log(college)
+    let round=appRound.value
+    console.log(round)
+    let plan = appPlan.value
+    console.log(plan)
 
-    fetch(`http://localhost:3000/${collegeName.value}`)
+    fetch(`http://localhost:3000/${college}`)
     .then( res => res.json())
     .then(data => {
         
@@ -29,30 +25,24 @@ document.addEventListener('DOMContentLoaded', () =>{
         
         cover.innerHTML +=`
         <div class="box">
-                <img src="${data[0].img}" alt="${collegeName.value}">
+                <img src="${data[0].img}" alt="${college}">
                 <div id="description">
                     <h2>${data[0].name}</h2>
                     <h3>${data[0].location}</h3>
                     <p>Acceptance Rate: ${data[0].acceptanceRate}%</p>
                     <a href=${data[0].weblink}>Click to see website</a>
-                    <p>I intend to apply: ${appRound.value}</p>
-                    <p>Application Plan: ${appPlan.value}</p>
-                    <button type='button' id='Btn'>Remove From List</button>
+                    <p>I intend to apply: ${round}</p>
+                    <p>Application Plan: ${plan}</p>
+                    <button type='click' id='${college}' class='Btn'>Remove From List</button>
                 </div>
         </div>
         `
+
     }
     )
     .catch( (error) => {
         alert(error);
     })
-
-    form.reset();
-    
    }
-
-   
-
-
 
 })
